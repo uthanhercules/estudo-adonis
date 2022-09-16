f4e9bf996ad90ad2612a7750af41e8907cb728fd
Uthan H <uthan.hercules12@gmail.com>
2022-09-16 16:25:01 -0300

    Comments is now showing on get all moments and get a moment by id

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -11,7 +11,7 @@ export default class MomentsController {
   }
 
   public async index() {
-    const moments = await Moment.all()
+    const moments = await Moment.query().preload('comments')
 
     return {
       data: moments,
@@ -20,6 +20,8 @@ export default class MomentsController {
 
   public async show({ params }: HttpContextContract) {
     const moment = await Moment.findOrFail(params.id)
+
+    await moment.load('comments')
 
     return {
       data: moment,
